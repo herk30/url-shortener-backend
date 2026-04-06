adecd1acbd3f8270aefeaa82efee3e0650b0d99b
Lele <Khoi.lenguyen3010@hcmut.edu.vn>
2026-04-06 19:39:58 +0700

    Fix backend

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const cors = require("cors"); 
+require("dotenv").config();
+
 const app = express();
+app.use(express.json());
+app.use(cors());
 app.use(express.json());
 
 // TODO: replace with your own MongoDB connection string
+const mongoURI = process.env.MONGODB_URI;
+mongoose.connect(mongoURI)
+  .then(() => console.log("Connect"))
+  .catch((err) => console.log("Error", err));
 mongoose.connect("mongodb://127.0.0.1:27017/url_shortener");
 
 const urlSchema = new mongoose.Schema({
@@ -48,5 +57,5 @@ app.get("/:shortCode", async (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+  console.log(`Server running on ${PORT}`);
 });
